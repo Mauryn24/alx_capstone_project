739ffbd98006f1cb60f505b12f612e63925139f3
Mauryn24 <johndoe@example.com>
2023-10-15 21:55:40 +0300

    fit psyche application

--- app.py
+++ app.py
@@ -1,17 +1,30 @@
+from flask import Flask, render_template, url_for
 # Import the Flask class from the flask module
-from flask import Flask
 
-# Import the render_template function from the flask module
-from flask import render_template
 
 # Create a Flask application instance
 app = Flask(__name__)
 
+# Configure the static files
+app.static_url_path = '/static'  # URL path for static files
+app.static_folder = 'static'     # Folder where static files are located
+
 # Define a route for the root URL ("/")
 @app.route('/')
-@app.route('/home')
 def home():
     return render_template('home.html')
 
-if (__name__ == '__main__'):
+@app.route('/profile/')
+def profile():
+    return render_template('profile.html')
+
+@app.route('/dashboard/')
+def dashboard():
+    return render_template('dashboard.html')
+
+@app.route('/timeline/')
+def timeline():
+    return render_template('timeline.html')
+
+if __name__ == '_main_':
     app.run(debug=True)
