5d4375a2ba14f239a629b027efe6bdb49d861ef6
Mauryn24 <johndoe@example.com>
2023-10-22 23:34:23 +0300

    deleting functionality

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,12 @@
 from app import app, db, bcrypt
 # import app decorators from app pakage
 
-from flask import render_template, url_for, flash, redirect, request
+from flask import render_template, url_for, flash, redirect, request, session
 # Import the Flask class from the flask module
 
 # Import User and Workout models from models.py
-from app.models import User, Workout  # Import User and Workout models from models.py
+# Import User and Workout models from models.py
+from app.models import User, Workout
 
 from flask_login import login_user, current_user, logout_user, login_required
 
@@ -19,6 +20,7 @@ from flask import jsonify
 def home():
     return render_template('home.html')
 
+
 @app.route('/submit/', methods=['POST'])
 def submit():
     name = request.form['fullname']
@@ -27,37 +29,46 @@ def submit():
     confirm = request.form['confirm_password']
     password = request.form['password']
     user = User(
-    fullname=name,
-    email=email,
-    phone = phone,
-    confirm = confirm,
-    password_hash=bcrypt.generate_password_hash(password)
-        )
+        fullname=name,
+        email=email,
+        phone=phone,
+        confirm=confirm,
+        password_hash=bcrypt.generate_password_hash(password)
+    )
     db.session.add(user)
     db.session.commit()
     flash('User created successfully!', 'success')
     return redirect(url_for('login'))
+
 
 @app.route('/profile/')
 @login_required
 def profile():
     return render_template('profile.html')
 
+
 @app.route('/dashboard/')
 @login_required
 def dashboard():
     return render_template('dashboard.html')
 
+
 @app.route('/timeline/')
 @login_required
 def timeline():
-    info = Workout.query.all()
-    print (info)
+    # info = Workout.query.all()
+    if current_user.is_authenticated:
+        user_id =  current_user.id  # Get the current user's ID
+
+        # Query workouts for the current user
+        info = Workout.query.filter_by(user_id=user_id).all()
+
+        return render_template('timeline.html', info=info)
     return render_template('timeline.html')
+
 
 @app.route('/api/workouts/', methods=['GET', 'POST'])
 def get_workouts():
-    # user_id = session.get('user_id')  # Retrieve the user's ID from the session
 
     if request.method == 'POST':
         exercise = request.form.get('exercise')
@@ -72,53 +83,87 @@ def get_workouts():
 
         print('user_id', user_id)
         print('date', date)
-        workout = Workout(user_id=user_id, exercise=exercise, sets=sets, reps=reps, weight=weight, duration=duration, date=date, calories_burned=calories_burned)
+        workout = Workout(user_id=user_id, exercise=exercise, sets=sets, reps=reps,
+                          weight=weight, duration=duration, date=date, calories_burned=calories_burned)
         db.session.add(workout)
         db.session.commit()
 
-        # Retrieve all workout data
-        info = Workout.query.all()
-        return render_template('timeline.html', info=info)
+        # Assuming current_user is logged in (you're using Flask-Login)
+    if current_user.is_authenticated:
+        user_id =  current_user.id  # Get the current user's ID
 
-    # return render_template('timeline.html')
+        # Query workouts for the current user
+        info = Workout.query.filter_by(user_id=user_id).all()
+
+        return render_template('timeline.html', info=info)
+    else:
+        return jsonify({'error': 'User not logged in'}), 404
 
 @app.route('/auth/login/', methods=['GET', 'POST'])
 def login():
-    #check if user is already logged in
+    # check if user is already logged in
     if current_user.is_authenticated:
-        return redirect(url_for('dashboard')) 
-    
+        return redirect(url_for('dashboard'))
+
     # if user is not logged in
     if request.method == 'POST':
         email = request.form['email']
         password = request.form['password']
-        #check if their email == to form.email.data
+        # check if their email == to form.email.data
         user = User.query.filter_by(email=email).first()
         # if user exists and the data he/she entered exists in the db
         if user and bcrypt.check_password_hash(user.password_hash, password):
             login_user(user)
-            
-            
+
             # check this line
             next_page = request.args.get('next')
             return redirect(next_page) if next_page else redirect(url_for('dashboard'))
            # return  redirect(url_for('dashboard'))
         else:
             flash('Login failed. Please check your credentials.', 'danger')
-    else:return render_template('auth/login.html')
+    else:
+        return render_template('auth/login.html')
+
 
 @app.route('/auth/signUp/')
-def signUp():    
+def signUp():
     return render_template('auth/signUp.html')
 
 # logout route
+
+
 @app.route('/logout/')
 def logout():
     logout_user()
     return redirect(url_for('home'))
 
 # accounts route
+
+
 @app.route('/account')
 @login_required
 def account():
     return render_template('acccount.html', title='Account')
+
+@app.route('/edit_workout/<int:id>', methods=['GET', 'POST'])
+def edit_workout(id):
+    info = Workout.query.get(id)
+    if request.method == 'POST':
+        info.exercise = request.form['exercise']
+        info.sets = request.form['sets']
+        info.reps = request.form['reps']
+        info.duration = request.form['duration']
+        info.date = request.form['date']
+        db.session.commit()
+        flash('Workout updated successfully!', 'success')
+        return redirect(url_for('timeline'))
+    return render_template('timeline.html', info=info)
+
+@app.route('/delete_workout/<int:workout_id>')
+def delete_workout(workout_id):
+    
+    info = Workout.query.get(workout_id)
+    db.session.delete(info)
+    db.session.commit()
+    flash('Workout deleted successfully!', 'success')
+    return redirect(url_for('timeline'))
