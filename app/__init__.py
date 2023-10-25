daf30c03b618b949b3098c96fb0268a2031a4018
Mauryn24 <johndoe@example.com>
2023-10-25 15:24:20 +0300

    nitialize the Flask application and its extensions.

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,4 @@
+# Import necessary modules and extensions for the Flask application
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, LoginManager
@@ -15,26 +16,25 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # Initialize the database
 db = SQLAlchemy(app)
 
-# Initialize Flask-Migrate
+# Initialize Flask-Migrate for database migrations
 migrate = Migrate(app, db)
 
-# initialize the database
+# Custom command to initialize the database using Flask CLI
 @app.cli.command()
 def init_db():
     """Initialize the database."""
     db.create_all()
 
-#
+# Push the application context to make it available
 app.app_context().push()
 
+# Initialize Bcrypt for password hashing
 bcrypt = Bcrypt(app)
 
-# Import the login function from flask_login
+# Initialize the LoginManager for user authentication
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'  # Change 'auth.login' to your actual login route
-
-# login_manager.login_message_category = 'info'
 
 # Configure the static files
 app.static_url_path = '/static'
@@ -43,11 +43,8 @@ app.static_folder = 'static'
 # Import routes after initializing extensions (for better organization)
 from app import routes
 
-
-# def create_app():
-#     app = Flask(__name__)
-
 # Generate a secret key for the Flask application
 secret_key = secrets.token_hex(32)
-# Set the secret key for the Flask application
-app.config['SECRET_KEY'] = 'your_secret_key' 
+
+# Set the secret key for the Flask application (make sure to use the generated secret key)
+app.config['SECRET_KEY'] = 'your_secret_key'
